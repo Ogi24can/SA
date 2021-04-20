b0c194dc86ae622b48d0b1e4b4ff807d75da6c30
Ogi24can <81964910+Ogi24can@users.noreply.github.com>
2021-04-20 12:12:56 +0300

    Add files via upload

---
+++ CRUD/CRUD/IDataAccessProvider.cs
@@ -0,0 +1,17 @@
+﻿using CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.DataAccess
+{
+    public interface IDataAccessProvider
+    {
+        void AddPatientRecord(Patient patient);
+        void UpdatePatientRecord(Patient patient);
+        void DeletePatientRecord(string id);
+        Patient GetPatientSingleRecord(string id);
+        List<Patient> GetPatientRecords();
+    }
+}
